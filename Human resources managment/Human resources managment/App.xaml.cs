f086e4c539c54527ed0ff0f8a5987c9e853c2163
EinRoterStern <ppapa9141@gmail.com>
2025-10-08 16:47:29 +0300

    Создание проекта

---
+++ Human resources managment/Human resources managment/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Human_resources_managment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
