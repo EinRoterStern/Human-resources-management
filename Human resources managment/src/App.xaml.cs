f5a6fb1099926d0250897d603ad4f504468cc799
EinRoterStern <ppapa9141@gmail.com>
2025-11-15 15:55:48 +0300

    Подключчение бд

--- Human resources managment/src/App.xaml.cs
+++ Human resources managment/src/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Configuration;
+﻿using Human_resources_managment.PostgresDataBase;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +13,25 @@ namespace Human_resources_managment
     /// </summary>
     public partial class App : Application
     {
+
+        public static IServiceProvider Services { get; private set; } = null!;
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //var config = new ConfigurationBuilder()
+            //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //.Build();
+
+            //var services = new ServiceCollection();
+
+            //var cs = config.GetConnectionString("DefaultConnection");
+
+            //services.AddDbContext<DBContextHRM>(opt =>
+            //opt.UseNpgsql(cs));
+
+        }
     }
 
 }
